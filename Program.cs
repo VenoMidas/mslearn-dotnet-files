3287355cacbcc0d9e057445db2662e7dc876b0cf
Mark Schumacher <schuma1022@gmail.com>
2022-12-28 06:34:33 -0600

    current directory and extensions

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
 IEnumerable<string> FindFiles(string folderName)
 {
     List<string> salesFiles = new List<string>();
@@ -9,19 +18,12 @@ IEnumerable<string> FindFiles(string folderName)
 
     foreach (var file in foundFiles)
     {
-        // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
     }
 
     return salesFiles;
-}
-
-var salesFiles = FindFiles("stores");
-
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
 }
